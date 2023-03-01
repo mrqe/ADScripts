d4a0304d1134d93759f157132e54452079e44d5a
mrqe <50217064+mrqe@users.noreply.github.com>
2023-03-01 11:38:20 -0500

    updated to resolve an error DifferenceObject

--- Get-Sox-Membership.ps1
+++ Get-Sox-Membership.ps1
@@ -1,9 +1,6 @@
 # Import the list of groups to check against
 $groupList = Import-Csv -Path "C:\Path\To\GroupList.csv"
 
-# Create an empty array to store the matching users
-$matchingUsers = @()
-
 # Get all users in Active Directory and iterate through them
 Get-ADUser -Filter * -Properties EmailAddress, DisplayName, MemberOf, Manager | ForEach-Object {
     # Get the user's information
@@ -21,20 +18,13 @@ Get-ADUser -Filter * -Properties EmailAddress, DisplayName, MemberOf, Manager | 
         $managerEmail = "itadmin@domain.com"
     }
 
-    # Check if any of the user's groups match the groups in the predefined list
-    $matchingGroups = Compare-Object -ReferenceObject $groupList.GroupName -DifferenceObject $userGroups -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}
+    # Check if the user is a member of any of the groups in the predefined list
+    if ($userGroups -ne $null -and $userGroups.Count -gt 0) {
+        $matchingGroups = Compare-Object -ReferenceObject $groupList.GroupName -DifferenceObject $userGroups -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}
 
-    # If there are matching groups, add the user to the matching users array
-    if ($matchingGroups) {
-        $matchingUser = [PSCustomObject]@{
-            Name = $displayName
-            Email = $userEmail
-            Groups = $userGroups -join ', '
-        }
-        $matchingUsers += $matchingUser
-
-        # Send an email to the user's manager
-        $messageBody = @"
+        # If there are matching groups, send an email to the user's manager
+        if ($matchingGroups) {
+            $messageBody = @"
 Hello,
 
 This message is to inform you that the following user is a member of one or more groups that match the predefined list:
@@ -49,9 +39,7 @@ $($matchingGroups.GroupName -join ', ')
 Thank you,
 Your IT team
 "@
-        Send-MailMessage -To $managerEmail -From "itadmin@domain.com" -Subject "User group membership notification" -Body $messageBody -SmtpServer "mail.domain.com"
+            Send-MailMessage -To $managerEmail -From "itadmin@domain.com" -Subject "User group membership notification" -Body $messageBody -SmtpServer "mail.domain.com"
+        }
     }
 }
-
-# Export the matching users to a CSV file
-$matchingUsers | Export-Csv -Path "C:\Path\To\MatchingUsers.csv" -NoTypeInformation
